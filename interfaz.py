e20b6ca1308171b90e164722a59cb5d7a6283494
José Sánchez <jose.venados6@gmail.com>
2020-04-25 14:14:28 -0600

    intefaz

--- interfaz.py
+++ interfaz.py
@@ -1,10 +1,61 @@
-from tkinter import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon, QPixmap
+from PIL import Image
 
-ventana = Tk()
 
-ventana.title("Analizador de lechugas")
-ventana.geometry('500x500')
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Analizador de Lechugas")
+        Dialog.resize(400, 300)
 
-ventana.Button(ventana, text='Seleccionar')
+        self.lblFoto = QtWidgets.QLabel(Dialog)
+        self.lblFoto.setGeometry(QtCore.QRect(10, 10, 150, 150))
+        self.lblFoto.setText("")
+        self.lblFoto.setObjectName("lblFoto")
 
-ventana.mainloop()
+        self.btnElegir = QtWidgets.QPushButton(Dialog)
+        self.btnElegir.setGeometry(QtCore.QRect(230, 20, 141, 23))
+        self.btnElegir.setObjectName("btnElegir")
+
+        self.btnEvaluar = QtWidgets.QPushButton(Dialog)
+        self.btnEvaluar.setGeometry(QtCore.QRect(230, 60, 141, 23))
+        self.btnEvaluar.setObjectName("btnEvaluar")
+
+        self.btnElegir.clicked.connect(self.seleccionarImagen)
+        self.btnEvaluar.clicked.connect(self.evaluarFoto)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+
+    def seleccionarImagen(self):
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.AnyFile)
+
+        if dlg.exec_():
+            filename = dlg.selectedFiles()
+
+        # Colocar imagen en lblFoto
+        imagen = QPixmap(str(filename[0])).scaled(150, 150)
+
+        self.lblFoto.setPixmap(imagen)
+
+    def evaluarFoto(self):
+        print('chipi')
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.btnElegir.setText(_translate("Dialog", "Seleccionar imagen"))
+        self.btnEvaluar.setText(_translate("Dialog", "Evaluar foto"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
